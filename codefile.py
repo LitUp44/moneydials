93fe2b23c4f355a5e52942bf6122226f5f470053
LitUp44 <litwiller.h@gmail.com>
2025-02-03 10:27:04 +1100

    Update codefile.py

--- codefile.py
+++ codefile.py
@@ -14,7 +14,7 @@ st.markdown(
 
 def app_header():
     """Display a persistent header with the logo and main title."""
-    st.image("alignedLogo.png", width=200)  # Adjust width as needed.
+    st.image("Horizontal Navy.png", width=200)  # Adjust width as needed.
     st.markdown(
         """
         <div style="background-color: #f5724b; padding: 10px; text-align: center; border-radius: 8px;">
