c95f5f0155afb6b3fb9713b5d2e73ae2bf37d343
LitUp44 <litwiller.h@gmail.com>
2025-02-03 10:01:37 +1100

    Update codefile.py

--- codefile.py
+++ codefile.py
@@ -7,7 +7,7 @@ def app_header():
         <div style="background-color: #f5724b; padding: 10px; text-align: center; border-radius: 8px;">
             <img src="logo.png" alt="Logo" style="height: 60px; margin-bottom: 10px;">
             <h1 style="color: black; margin: 0;">What are your Money Dials?</h1>
-            <p style="color: black; margin: 5px 0 0 0; font-size: 16px;">Where do you get the most JOY spending money?</p>
+            <p style="color: black; margin: 5px 0 0 0; font-size: 32px;">Where do you get the most JOY spending money?</p>
         </div>
         """, unsafe_allow_html=True
     )
