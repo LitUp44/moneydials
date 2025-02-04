10e191d08b30d69365ebae3d05f00bc3181d3729
LitUp44 <litwiller.h@gmail.com>
2025-02-04 18:50:11 +1100

    Update codefile.py

--- codefile.py
+++ codefile.py
@@ -437,7 +437,7 @@ def show_final_results():
         insight_message = (
             insight_style +
             "<strong>Insight:</strong><br>"
-            "It looks like your ideal spending on your top money dials is more than how much you currently are.  "
+            "It looks like your you're spending less on your TOP money dials than you wish you were.  "
             "This is a great financial goal to work towards! It may not always be possible, but it's worth thinking about "
             "if you can free up money from somewhere you care about less to be able to spend more on these!"
             "</div>"
